40d413fae3e6713855ca961f04053ec73cede128
kaverii1605-art <kaverii1605@gmail.com>
2026-05-12 13:34:09 +0530

    updated email service

--- src/main/java/com/blueaxis/blueaxisapi/services/EmailService.java
+++ src/main/java/com/blueaxis/blueaxisapi/services/EmailService.java
@@ -1,26 +1,47 @@
 package com.blueaxis.blueaxisapi.services;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.mail.SimpleMailMessage;
-import org.springframework.mail.javamail.JavaMailSender;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
-
-
+import org.springframework.web.client.RestTemplate;
 
 @Service
 public class EmailService {
 
-	
-	  @Autowired
-	    private JavaMailSender mailSender;
+    @Value("${BREVO_API_KEY}")
+    private String brevoApiKey;
 
-	    public void sendEmail(String to, String subject, String text) {
+    public void sendEmail(String to, String subject, String text) {
 
-	        SimpleMailMessage message = new SimpleMailMessage();
-	        message.setTo(to);
-	        message.setSubject(subject);
-	        message.setText(text);
+        String url = "https://api.brevo.com/v3/smtp/email";
 
-	        mailSender.send(message);
-	    }
+        
+        System.out.println("BREVO API EMAIL SERVICE HIT");
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        headers.set("api-key", brevoApiKey);
+
+        Map<String, Object> body = Map.of(
+            "sender", Map.of(
+                "name", "BlueAxis Media",
+                "email", "kaverii1605@gmail.com"
+            ),
+            "to", List.of(
+                Map.of("email", to)
+            ),
+            "subject", subject,
+            "htmlContent", "<p>" + text.replace("\n", "<br>") + "</p>"
+        );
+
+        HttpEntity<Map<String, Object>> request =
+                new HttpEntity<>(body, headers);
+
+        RestTemplate restTemplate = new RestTemplate();
+        restTemplate.postForEntity(url, request, String.class);
+    }
 }

fb2c4da3b4c1b56b89d3af9bf60f483593a24bb1
kaverii1605-art <kaverii1605@gmail.com>
2026-05-12 11:58:34 +0530

    updated

--- src/main/java/com/blueaxis/blueaxisapi/controllers/Usercotroller.java
+++ src/main/java/com/blueaxis/blueaxisapi/controllers/Usercotroller.java
@@ -181,8 +181,9 @@ public class Usercotroller {
 
 	            String liveLink = "https://newdeployment-two.vercel.app/reset-password.html?token=" + token;
 	            String message = "Reset Password:\n" + liveLink;
-
-//	            emailService.sendEmail(email, "Reset Your Password", message);
+	            emailService.sendEmail(email, "Reset Your Password", message);
+	            
+	            
 
 	            response.put("message", "Reset password link sent");
 	            return ResponseEntity.ok(response);
